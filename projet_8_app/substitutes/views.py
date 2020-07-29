4fb1c2358651eb197eb8a56be67e1a7ed383a4db
Alexandre Prieto Pantoja <alexandreprietopantoja@MacBook-Pro.local>
2020-07-29 21:20:01 +0200

    modify paginator

--- projet_8_app/substitutes/views.py
+++ projet_8_app/substitutes/views.py
@@ -16,53 +16,53 @@ class SubstituteView(View):
 
     def get(self, request):        
               
-        # page = request.GET.get('page', 1)     
-        # if 'product' not in self.context:
-        #     product_name = request.GET.get('product')        
-        #     product, substitute_list = find_substitute(product_name)
-        #     num_substitutes = len(substitute_list)
+        page = request.GET.get('page', 1)     
+        if 'product' not in self.context:
+            product_name = request.GET.get('product')        
+            product, substitute_list = find_substitute(product_name)
+            num_substitutes = len(substitute_list)
             
-        #     paginator = Paginator(substitute_list, 6)
+            paginator = Paginator(substitute_list, 6)
 
-        #     self.context['product'] = product
+            self.context['product'] = product
             
-        #     self.context['num_substitutes'] = num_substitutes
+            self.context['num_substitutes'] = num_substitutes
 
-        # else:
-        #     paginator = Paginator(self.context['product'], 6)        
+        else:
+            paginator = Paginator(self.context['product'], 6)        
                 
-        # try:
-        #     substitutes = paginator.page(page)
-        # except PageNotAnInteger:
-        #     substitutes = paginator.page(1)
-        # except EmptyPage:
-        #     substitutes = paginator.page(paginator.num_pages)
+        try:
+            substitutes = paginator.page(page)
+        except PageNotAnInteger:
+            substitutes = paginator.page(1)
+        except EmptyPage:
+            substitutes = paginator.page(paginator.num_pages)
         
-        # self.context['substitute_list'] = substitutes
+        self.context['substitute_list'] = substitutes
 
-        # return render(request, self.template_name, self.context)
+        return render(request, self.template_name, self.context)
 
 
           
        
-        product_name = request.GET.get('product')        
-        product, substitute_list = find_substitute(product_name)
-        num_substitutes = len(substitute_list)
-        
-        page = request.GET.get('page', 1)   
-        paginator = Paginator(substitute_list, 6)       
+        # product_name = request.GET.get('product')        
+        # product, substitute_list = find_substitute(product_name)
+        # num_substitutes = len(substitute_list)
+
+        # page = request.GET.get('page', 1)   
+        # paginator = Paginator(substitute_list, 6)       
                 
-        try:
-            substitute_pages = paginator.page(page)
-        except PageNotAnInteger:
-            substitute_pages = paginator.page(1)
-        except EmptyPage:
-            substitute_pages = paginator.page(paginator.num_pages)
+        # try:
+        #     substitute_pages = paginator.page(page)
+        # except PageNotAnInteger:
+        #     substitute_pages = paginator.page(1)
+        # except EmptyPage:
+        #     substitute_pages = paginator.page(paginator.num_pages)
         
-        self.context['product'] = product
-        self.context['num_substitutes'] = num_substitutes   
+        # self.context['product'] = product
+        # self.context['num_substitutes'] = num_substitutes   
         
-        self.context['substitute_list'] = substitute_list
-        self.context["substitute_pages"] = substitute_pages 
+        # self.context['substitute_list'] = substitute_list
+        # self.context["substitute_pages"] = substitute_pages 
                        
         return render(request, self.template_name, self.context)
