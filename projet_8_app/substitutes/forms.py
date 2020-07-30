349d34b180b5e10c726ab82f69b26c5e91495914
Alexandre Prieto Pantoja <alexandreprietopantoja@MacBook-Pro.local>
2020-07-30 12:40:51 +0200

    update search bar

--- projet_8_app/substitutes/forms.py
+++ projet_8_app/substitutes/forms.py
@@ -3,6 +3,6 @@ from django import forms
 class SubstituteSearchForm(forms.Form):
     product = forms.CharField(label='', max_length=100,
                                     widget=forms.TextInput(
-                                        attrs={'class': 'search-product', 'placeholder': 'Recherche...',
-                                        'name': 'search'}
+                                        attrs={'class': 'form-control my-0 py-1 lime-border', 'placeholder': 'Recherche...',
+                                        'name': 'search', 'type':'text'}
                                     ))
