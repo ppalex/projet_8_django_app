e85a323a600baaef2caa9f8481dbde1c04874c86
Alexandre Prieto Pantoja <alexandreprietopantoja@MacBook-Pro.local>
2020-08-03 20:49:51 +0200

    test save form

--- tests/unit/substitutes/test_forms.py
+++ tests/unit/substitutes/test_forms.py
@@ -1,13 +1,51 @@
 from django.test import TestCase
-
+from core.models.user import User
+from core.models.product import Product
 
 from substitutes.forms import SubstituteSearchForm
+from django.urls import reverse
 
 class SubstituteSearchFormTest(TestCase):
     
     def test_renew_form_date_field_label(self):
         form = SubstituteSearchForm()
         self.assertTrue(form.fields['product'].label == '')
+
+
+class SaveSubstituteFormTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        user1 = User.objects.create_user(username='testuser1', password='1X<ISRUkw+tuK')
+
+
+        number_of_product = 1
+
+        for product_id in range(number_of_product):
+            product =  Product.product_objects.create_product(barcode=product_id,
+                                                    product_name=f"product_name_{product_id}",
+                                                    nutriscore_grade="A",
+                                                    product_description="Product description",
+                                                    off_url="www.off.com",
+                                                    image_url="www.image_url.com")
+            user1.product_set.add(product)
+
+
+    def test_save_submit_button(self):
+        login = self.client.login(username='testuser1', password='1X<ISRUkw+tuK')
+
+        response = self.client.post(self.client.get(reverse('favourite')), {'save_substitute':'Sauvegarder'})
+
+        response = self.client.get(reverse('favourite'))
+        self.assertEqual(response.status_code, 200)
+
+        
+
+
+        
+
+    
+
     
     
     
---
+++ tests/unit/user/test_views.py
@@ -0,0 +1,58 @@
+from django.test import TestCase
+from django.urls import reverse
+
+
+from core.models.product import Product
+
+class RegisterViewTest(TestCase):
+    
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/register/')
+
+        self.assertEqual(response.status_code, 200)
+
+    
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('register'))
+
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('register'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/register.html')
+
+        
+
+class LoginViewTest(TestCase):
+    
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/login/')
+
+        self.assertEqual(response.status_code, 200)
+
+    
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('login'))
+
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('login'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/login.html')
+
+        
+
+
+
+
+
+
+
+
+    
