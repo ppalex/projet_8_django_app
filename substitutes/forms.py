715d763f13ae83b9447c0db4643cdd04a4f575b0
ppalex <prietopantoja.alex@gmail.com>
2020-08-03 13:05:01 +0200

    add empty 404, 500 template

--- substitutes/forms.py
+++ substitutes/forms.py
@@ -1,8 +1,10 @@
 from django import forms
 
+
 class SubstituteSearchForm(forms.Form):
-    product = forms.CharField(label='', max_length=100,
-                                    widget=forms.TextInput(
-                                        attrs={'class': 'form-control my-0 py-1 lime-border', 'placeholder': 'Recherche...',
-                                        'name': 'search', 'type':'text'}
-                                    ))
+    product = forms.CharField(label='', 
+                                max_length=100,
+                                widget=forms.TextInput(
+                                    attrs={'class': 'form-control my-0 py-1 lime-border', 
+                                        'placeholder': 'Recherche...',
+                                        'name': 'search', 'type':'text'}))
