bb5e3773037f55d39f56326e6972a74822b23ba4
ppalex <prietopantoja.alex@gmail.com>
2020-08-03 12:44:25 +0200

    add first test form

--- tests/unit/substitutes/test_forms.py
+++ tests/unit/substitutes/test_forms.py
@@ -5,4 +5,9 @@ from substitutes.forms import SubstituteSearchForm
 
 class SubstituteSearchFormTest(TestCase):
     
+    def test_renew_form_date_field_label(self):
+        form = SubstituteSearchForm()
+        self.assertTrue(form.fields['product'].label == '')
+    
+    
     
