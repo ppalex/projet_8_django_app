1a7a49a7af9b5044458879b8d3b44a31b1fbbfda
Alexandre Prieto Pantoja <alexandreprietopantoja@MacBook-Pro.local>
2020-08-19 12:46:37 +0200

    update register

--- users/forms.py
+++ users/forms.py
@@ -1,9 +1,24 @@
 from django.contrib.auth.forms import UserCreationForm
 from core.models.user import User
+from django import forms
 
 
 class CustomUserCreationForm(UserCreationForm):
+    """This class represents the user register form.
+    """
 
     class Meta(UserCreationForm):
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+    def clean_username(self):
+        username = self.cleaned_data.get("username")
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError("Le nom d'utilisateur existe déjà")
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("L'emai existe déjà")
+        return email
--- users/views.py
+++ users/views.py
@@ -9,18 +9,21 @@ from .forms import CustomUserCreationForm
 
 
 class RegisterView(View):
+    """This class displays the register view.
+    """
 
     register_form = CustomUserCreationForm
     template_name = 'users/register.html'
 
     def get(self, request):
-        form = self.register_form(request.GET)
+        form = self.register_form()
         context = {'form': form}
 
         return render(request, self.template_name, context)
 
     def post(self, request):
         form = self.register_form(request.POST)
+        context = {'form': form}
 
         if form.is_valid():
 
@@ -34,22 +37,26 @@ class RegisterView(View):
 
             return redirect('index')
 
-        return self.get(request)
+        return render(request, self.template_name, context)
 
 
 class CustomLoginView(SuccessMessageMixin, LoginView):
-
+    """This class displays the login view.
+    """
     success_url = '/'
     success_message = "Vous êtes connectés %(username)s"
 
 
 class CustomLogoutView(SuccessMessageMixin, LogoutView):
-
+    """This class is used to logout the user.
+    """
     success_url = '/'
     success_message = "Vous êtes déconnectés"
 
 
 class ProfileView(View):
+    """This class displays the profile view.
+    """
     template_name = 'users/profile.html'
 
     def get(self, request):
